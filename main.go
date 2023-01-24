d5a2b4b67fbfaac165ab0e406e5ff3cdcdd98b46
fitz <danilomarques20@hotmail.com>
2023-01-24 19:15:38 -0300

    getting current working directory.

--- main.go
+++ main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/danilomarques1/effingo/cmd"
 )
 
 var (
-	dir          = flag.String("dir", ".", "The directory to search for duplicate files")
 	ignoreCache  = flag.Bool("ignore-cache", false, "Ignore the use of the cached file")
 	shouldRemove = flag.Bool("remove", false, "If should remove the files")
 )
 
 func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dir := flag.String("dir", cwd, "The directory to search for duplicate files")
 	flag.Parse()
 
 	traverser, err := cmd.NewDirTraverser(*dir, *ignoreCache, *shouldRemove)
